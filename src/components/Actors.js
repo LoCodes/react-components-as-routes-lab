ee7f447301d6c1335d80825ba1275eee6802cbaa
Loren Cipriano <loren25cipriano@gmail.com>
2021-08-17 16:29:55 -0700

    learn routing

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+  const actorsItem = actors.map((actor, index) => <div key={index}>
+    Name: {actor.name}
+    Movies: {actor.movies.map(movie => <li>{movie}</li>)}
+  </div>)
   return (
     <div>
       {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorsItem}
     </div>
   );
 };
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,9 +2,15 @@ import React from 'react';
 import { directors } from '../data';
 
 const Directors = () => {
+  const directorItems = directors.map((director, index) => <div key={index}>
+  Name: {director.name}
+  Movies: {director.movies.map(movie => <li>{movie}</li>)}
+  </div>)
   return (
     <div>
       {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directorItems}
     </div>
   );
 }
